f53e2625163fdb1e68a4bc127ddb6e0c03e4a032
ryongseong <xmssnsk2119@gmail.com>
2024-01-11 15:25:32 +0900

    230111

--- Part4/ch2/server.js
+++ Part4/ch2/server.js
@@ -1,37 +1,33 @@
 const express = require('express');
+const usersRouter = require('./routes/users.router');
+const postsRouter = require('./routes/posts.router');
+const path = require('path');
 
 const PORT = 4000;
-
 const app = express();
 
-const Users = [
-    {
-        id:0,
-        name: 'Jack'
-    },
-    {
-        id:1,
-        name: 'Jennifer'
-    }
-]
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+    const start = Date.now();
+    console.log(`start: ${req.method} ${req.url}`);
+    next();
+    const diffTime = Date.now() - start;
+    console.log(`end  : ${req.method} ${req.baseUrl}${req.url} ${diffTime}ms`);
+})
+
+app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.get('/users', (req, res) => {
-    res.send(Users);
+    res.render('index', {
+        imageTitle: "It is a forest2"
+    })
 })
-app.get('/users/:userId', (req, res) => {
-   const userId = Number(req.params.userId);
-   const user = Users[userId];
-   if (user) {
-    res.json(user);
-   } else {
-    res.sendStatus(404);
-   }
-});
-
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
